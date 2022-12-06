0438e8edad1aaa53767bb42660673a6fcd27fc3d
daniyalinayat <daniyal.inayatullah@virtual-identity.com>
2022-12-06 14:42:21 +0100

    fix: env file

--- config/openai.js
+++ config/openai.js
@@ -1,13 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  
+  organization: process.env.ORG_ID,
+  apiKey: process.env.API_KEY,
 });
 
 export const openai = new OpenAIApi(configuration);
-
-// const completion = await openai.createCompletion({
-//   model: "text-davinci-002",
-//   prompt: "Hello world",
-// });
-// console.log(completion.data.choices[0].text);

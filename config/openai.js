d93e14968548ff7ae7ab8573d13ebe1b229cefc3
daniyalinayat <daniyal.inayatullah@virtual-identity.com>
2022-12-06 14:34:28 +0100

    commit

--- config/openai.js
+++ config/openai.js
@@ -1,8 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  organization:'org-qSTMZ6ASi3galKZXgZFNPuUi',
-  apiKey: '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',
+  
 });
 
 export const openai = new OpenAIApi(configuration);
